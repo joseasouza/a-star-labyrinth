e7f851c6a64a82ddc501f62132f296727ea0ce15
Jose Victor <victor_rpg05@hotmail.com>
2016-03-25 00:21:30 -0300

    Adicionando pagina sobre o sistema, traduzindo para o ingles e corrigndo problemas com adsense

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # a-star-labyrinth
-Projeto desenvolvido para uso na disciplina de Controle Inteligente disciplinada pelo professor Fabio Meneghetti Ugulino de Araujo - DCA / UFRN.
+This application is a project developed for the subject of 'Intelligent Control' 
+in the undergraduate course of Computer Engineering, taught by 
+the Prof. Fábio Meneghetti Ugulino de Araújo 
+at Universidade Federal do Rio Grande do Norte, UFRN. 
+The project is an implementation of the A* Pathfinding, using HTML5, JavaScript and jQuery Plugins.
 
-Trabalho foi desenvolvido utilizando HTML5, CSS3 e JavaScript
+<a href="http://dudevictor.github.io/a-star-labyrinth">You can see it online here.</a><br><br>
+
+For more details, 
+<a class="highlight" href="http://dudevictor.github.io/projects/controle-inteligente/">see this topic</a>.
+
+Any questions or doubts, please contact me.
--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
 /*
+ * Apache License 2.0
  * Copyright (c) 2016 - José Victor Alves de Souza - https://github.com/dudevictor/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 /**
  * This script defines the App function and all logic to control and build the labyrinth.
@@ -10,7 +24,7 @@ var App = function() {
 
     var canvas = document.getElementById("canvas");
     var minCanvasWidth = 981;
-    var minCanvasHeight =  535;
+    var minCanvasHeight =  500;
     var game;
     var aStar;
     var labBuilder;
@@ -92,8 +106,8 @@ var App = function() {
 
     $("#play").on("click", function () {
         if(!labBuilder.isLabyrinthOk()) {
-            $("#modal-erro div[name='modal-message']").html("Você ainda não montou o labirinto corretamente! Verifique" +
-                " se você marcou as posições iniciais e finais!");
+            $("#modal-erro div[name='modal-message']").html("You have not set up the labyrinth correctly! " +
+                "Make sure you marked the start and end positions!!");
             $("#modal-erro").modal('show');
             return false;
         }
